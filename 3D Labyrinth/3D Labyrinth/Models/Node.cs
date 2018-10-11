2a65d9516301161090f10b51c108c3c009840eb6
Fabiandw <fabian.de.wilde94@gmail.com>
2018-10-11 11:49:45 +0200

    Node class added, old textures removed

---
+++ 3D Labyrinth/3D Labyrinth/Models/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Node
+    {
+        private double _x;
+        private double _y;
+        private double _z;
+        public double x { get { return _x; } }
+        public double y { get { return _y; } }
+        public double z { get { return _z; } }
+        public string code { get; }
+
+        public Node(string value, double x, double y, double z)
+        {
+            //Benodigd voor aanmaken
+            _x = x;
+            _y = y;
+            _z = z;
+            this.code = value;
+        }
+    }
+}
