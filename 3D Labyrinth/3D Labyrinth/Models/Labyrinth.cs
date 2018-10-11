3c83d0fe777ecc5f5f6282888a65b856677161aa
Fabiandw <fabian.de.wilde94@gmail.com>
2018-10-11 13:02:26 +0200

    Labyrinth class

---
+++ 3D Labyrinth/3D Labyrinth/Models/Labyrinth.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Labyrinth
+{
+        public Guid guid { get; set; }
+        public List<Node> nodeList { get; set; }
+        public List<Connection> connectionList { get; set; }
+        public Grid grid { get; set; }
+
+        public Labyrinth(List<Node> nodes, List<Connection> connections, Grid thisGrid)
+        {
+            nodeList = nodes;
+            connectionList = connections;
+            grid = thisGrid;
+            guid = Guid.NewGuid();
+        }
+        public void RemoveWalls()
+        {
+
+        }
+    }
+}
