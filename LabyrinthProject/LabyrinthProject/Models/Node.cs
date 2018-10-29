760d22710e33a103cfd239e78443c65f1b1375f0
Daniel <hove1500@student.nhl.nl>
2018-10-29 16:14:05 +0100

    16;13 smol fix

--- LabyrinthProject/LabyrinthProject/Models/Node.cs
+++ LabyrinthProject/LabyrinthProject/Models/Node.cs
@@ -66,13 +66,6 @@ namespace LabyrinthProject.Models
             //Return null, or a random unvisited node
             return returnNode;
         }
-
-        public void ConnClear()
-        {
-            foreach (Node node in connectedNodeList)
-            {
-                this.connectedNodeList.Clear();
-            }
-        }
+    
     }
 }
